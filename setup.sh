aca3a89b1f176b2221ed434a635205ba85b3f79a
sap1ens <sapiensy@gmail.com>
2016-04-10 17:57:46 -0700

    Update setup script

--- setup.sh
+++ setup.sh
@@ -23,3 +23,8 @@ _checkout_repo_into_folder ~/.oh-my-fish "oh-my-fish/oh-my-fish" 403e5652b428331
 _checkout_repo_into_folder ~/.vim/bundle/Vundle.vim "gmarik/Vundle.vim" cfd3b2d388a8c2e9903d7a9d80a65539aabfe933
 _checkout_repo_into_folder ~/.vim/bundle/nerdtree "scrooloose/nerdtree" 96c07746b46c12d0aca7a5276cc5bd0a260b82fe
 _checkout_repo_into_folder ~/.vim/bundle/vim-colors-solarized "altercation/vim-colors-solarized" 528a59f26d12278698bb946f8fb82a63711eec21
+
+
+# Installing brew stuff
+brew tap Homebrew/bundle
+brew bundle
