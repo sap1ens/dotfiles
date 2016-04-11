c73224b9acb83b94699a28e03ea9fbd03df56ad1
Yaroslav Tkachenko <sapiensy@gmail.com>
2016-04-11 09:32:09 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ _checkout_repo_into_folder() {
 _checkout_repo_into_folder ~/.oh-my-fish "oh-my-fish/oh-my-fish" 403e5652b428331286ded98c8579df3de39b24fb
 
 # Vim
+mkdir -p .vim/bundle
 _checkout_repo_into_folder ~/.vim/bundle/Vundle.vim "gmarik/Vundle.vim" cfd3b2d388a8c2e9903d7a9d80a65539aabfe933
 _checkout_repo_into_folder ~/.vim/bundle/nerdtree "scrooloose/nerdtree" 96c07746b46c12d0aca7a5276cc5bd0a260b82fe
 _checkout_repo_into_folder ~/.vim/bundle/vim-colors-solarized "altercation/vim-colors-solarized" 528a59f26d12278698bb946f8fb82a63711eec21
